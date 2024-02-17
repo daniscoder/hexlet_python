ca84b0ecdf56379f00e881cf9b789c53e36d0bf5
daniscoder <arslanovdk@gmail.com>
2024-02-17 15:28:35 +0500

    first commit

---
+++ arrays.py
@@ -0,0 +1,24 @@
+# 02
+# def is_list(value):
+#     return isinstance(value, list)
+
+
+# 03
+# import math
+#
+#
+# def get_square_roots(value):
+#     if value < 0:
+#         return []
+#     if value == 0:
+#         return [0]
+#     square_root = math.sqrt(value)
+#     return [-square_root, square_root]
+
+
+# # 04
+# def get_range(n):
+#     return list(range(n))
+
+
+#05
