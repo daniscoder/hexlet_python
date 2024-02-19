dbce5e64a5f0faf590c930ad4e14cff014427acb
Danis Arslanov <arslanovdk@gmail.com>
2024-02-19 15:30:02 +0500

    lessons complited

--- arrays.py
+++ arrays.py
@@ -1,9 +1,9 @@
-# Концепция списков
+# 2. Концепция списков
 # def is_list(value):
 #     return isinstance(value, list)
 
 
-# Создание списков и добавление элементов
+# 3. Создание списков и добавление элементов
 # import math
 #
 #
@@ -16,14 +16,61 @@
 #     return [-square_root, square_root]
 
 
-# Ссылки
+# 4. Ссылки
 # def get_range(n):
 #     return list(range(n))
 
 
-# Ссылки и изменяемость
+# 5. Ссылки и изменяемость
 # def duplicate(items):
 #     items.extend(items)
 
 
-# Модификация списков поэлементно
+# 6. Модификация списков поэлементно
+# def rotate(items):
+#     if items:
+#         items.insert(0, items.pop(-1))
+#
+#
+# items = [1, 2, 3]
+# rotate(items)
+# print(items)  # => [3, 1, 2]
+
+
+# 7. Срезы
+# def rotated_left(items):
+#     return items[1:] + items[:1]
+#
+#
+# def rotated_right(items):
+#     return items[-1:] + items[:-1]
+#
+#
+# print(rotated_left("ABCD"))
+# print(rotated_right([1, 2, 3, 4]))
+
+
+# 8. Цикл for
+def find_index(value, items):
+    for i, item in enumerate(items):
+        if value == item:
+            return i
+#
+#
+# find_index('t', 'cat')  # 2
+# find_index(5, [1, 2, 3, 4, 5, 6, 7])  # 4
+# find_index(42, [])  # True
+# find_index('!', 'abc')  # True
+
+
+# 9. Итераторы
+# def find_second_index(value, items):
+#     it = iter(items)
+#     first_index = find_index(value, it)
+#     second_index = find_index(value, it)
+#     if second_index is not None:
+#         return first_index + second_index + 1
+#
+#
+# print(find_second_index('b', 'bob'))
+# print(find_second_index('n', 'cannon'))
