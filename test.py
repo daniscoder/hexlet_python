bc984e6534da91502617ffff6f59862cb2d80358
Danis Arslanov <arslanovdk@gmail.com>
2025-04-03 23:59:56 +0500

    test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def move_zeros(arr):
+    return [x for x in arr if x != 0] + [0] * arr.count(0)
 
-print(range(len(index)))
+#test
+print(move_zeros([1, 0, 1, 2, 0, 1, 3]))  # Output: [1, 1, 2, 1, 3, 0, 0]
+print(move_zeros([0, 0, 1, 2, 3]))        # Output: [1, 2, 3, 0, 0, 0]
+print(move_zeros([0, 0, 0, 1]))           # Output: [1, 0, 0, 0]
+print(move_zeros([1, 2, 3]))              # Output: [1, 2, 3]
